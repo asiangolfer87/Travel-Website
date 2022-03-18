de9ab9f7335fc20f74baa13bcfc6b692ff07450d
daniel sidiki <45163595+asiangolfer87@users.noreply.github.com>
2022-03-18 14:47:07 -0500

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import HeroSection from "./HeroSection";
+
+import "../../App.css";
+import "./HeroSection.css";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
+
+export default Home;
